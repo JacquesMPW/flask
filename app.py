a62beea75d61253415a1f55cbb1f45882e0e3824
JacquesMPW <lon24deic01@mpw.ac.uk>
2025-03-10 15:29:34 +0000

    signup

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
+import sqlite3
+import os
+
+DATABASE = 'database.db'
 
 app = Flask(__name__)
+con = sqlite3.connect(DATABASE)
+cur = con.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS users(
+            username VARCHAR(20) PRIMARY KEY NOT NULL,     
+            password VARCHAR(20) NOT NULL)
+            """)
+con.commit()
+con.close()
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    if request.method== "GET":
-        return render_template("signup.html")
-    return "Signup succesful"
+    if request.method == 'GET':
+        return render_template('signup.html')
+    else:
+        cur.execute("""INSERT INTO users (username,password)
+        VALUES(?,?)""",(request.form['Username'],request.form['Password']));
+        con.commit()
+        con.close()
+    return "Signup Successful"
 
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template('index.html')
+        return render_template('signup.html')
     else:
         Username = request.form["Username"]
         Password = request.form["Password"]
