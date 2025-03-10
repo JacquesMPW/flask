ac3048ecbb00a7cca9c3da78041894e6f44baa67
JacquesMPW <lon24deic01@mpw.ac.uk>
2025-03-10 15:57:14 +0000

    signup

--- app.py
+++ app.py
@@ -1,29 +1,51 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3
-import os
 
 DATABASE = 'database.db'
 
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row 
+    return conn
+
 app = Flask(__name__)
-con = sqlite3.connect(DATABASE)
-cur = con.cursor()
+conn = get_db_connection
+cur = conn.cursor()
+
 cur.execute("""CREATE TABLE IF NOT EXISTS users(
             username VARCHAR(20) PRIMARY KEY NOT NULL,     
             password VARCHAR(20) NOT NULL)
             """)
-con.commit()
-con.close()
+conn.commit()
+conn.close()
+
+def signup_user(username, password):
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO users (id, username, email, first_name, last_name, password_hash)
+        VALUES (?, ?)
+    ''', (username, password))
+
+    conn.commit()
+    conn.close()
+
+    return 'Signup successful! Please log in.'
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    if request.method == 'GET':
-        return render_template('signup.html')
-    else:
-        cur.execute("""INSERT INTO users (username,password)
-        VALUES(?,?)""",(request.form['Username'],request.form['Password']));
-        con.commit()
-        con.close()
-    return "Signup Successful"
+    username = request.form['username']
+    password = request.form['password']  
+
+    message = signup_user(username, password)
+    flash(message)
+
+    if 'Signup successful!' in message:
+        return redirect(url_for('homepage'))
+    
+    return redirect(url_for('index'))
 
 @app.route("/", methods=["GET", "POST"])
 def login():
