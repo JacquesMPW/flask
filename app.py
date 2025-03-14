a2103aa4d5f40c58a26f2edaf72375dd00835465
JacquesMPW <lon24deic01@mpw.ac.uk>
2025-03-14 12:52:55 +0000

    login/signup

--- app.py
+++ app.py
@@ -1,60 +1,53 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3
+import requests
 
 DATABASE = 'database.db'
 
-def get_db_connection():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row 
-    return conn
 
 app = Flask(__name__)
-conn = get_db_connection
-cur = conn.cursor()
+con= sqlite3.connect(DATABASE)
+cur = con.cursor()
 
 cur.execute("""CREATE TABLE IF NOT EXISTS users(
             username VARCHAR(20) PRIMARY KEY NOT NULL,     
             password VARCHAR(20) NOT NULL)
             """)
-conn.commit()
-conn.close()
+con.commit()
+con.close()
 
-def signup_user(username, password):
-
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    cursor.execute('''
-        INSERT INTO users (id, username, email, first_name, last_name, password_hash)
-        VALUES (?, ?)
-    ''', (username, password))
-
-    conn.commit()
-    conn.close()
-
-    return 'Signup successful! Please log in.'
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    username = request.form['username']
-    password = request.form['password']  
-
-    message = signup_user(username, password)
-    flash(message)
-
-    if 'Signup successful!' in message:
-        return redirect(url_for('homepage'))
+    if request.method == "GET":
+        return render_template('signup.html')
+    else:
+        con = sqlite3.connect(DATABASE)
+        cur = con.cursor()
+        cur.execute("INSERT INTO users (username, password) VALUES (?, ?)", 
+                            (request.form["username"], request.form["password"]))
+        con.commit()
+        con.close()
+    return "Signup successful"
     
-    return redirect(url_for('index'))
 
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template('signup.html')
+        return render_template('index.html')
     else:
-        Username = request.form["Username"]
-        Password = request.form["Password"]
-        if  Password == "123" and Username == "bob":
-            return "Hello " + Username
-        else:
+        con = sqlite3.connect(DATABASE)
+        cur = con.cursor()
+        cur.execute("SELECT * FROM users WHERE username = ? AND password = ?", 
+                            (request.form["username"], request.form["password"]))
+        match = len(cur.fetchall())
+        con.commit()
+        con.close()
+        if  match == 0:
             return "Login Failed"
+        else:
+            return "Welcome" + request.form["username"]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
