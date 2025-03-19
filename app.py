05a8c1f45e8dbb6a7cb5517d09b5de43440819fa
JacquesMPW <lon24deic01@mpw.ac.uk>
2025-03-19 13:53:40 +0000

    s/l made

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import sqlite3
-import requests
+import hashlib
+
 
 DATABASE = 'database.db'
 
 
 app = Flask(__name__)
+app.secret_key = "Jacques"
 con= sqlite3.connect(DATABASE)
 cur = con.cursor()
 
@@ -17,6 +19,10 @@ con.commit()
 con.close()
 
 
+@app.route("/")
+def index():
+    return render_template('login.html')
+
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "GET":
@@ -24,29 +30,36 @@ def signup():
     else:
         con = sqlite3.connect(DATABASE)
         cur = con.cursor()
+        encoded = request.form["password"].encode()
+        hash = hashlib.sha256(encoded).hexdigest()
+        print(hash)
         cur.execute("INSERT INTO users (username, password) VALUES (?, ?)", 
-                            (request.form["username"], request.form["password"]))
+                            (request.form["username"],hash))
         con.commit()
         con.close()
     return "Signup successful"
     
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template('index.html')
+        return render_template('login.html')
     else:
         con = sqlite3.connect(DATABASE)
         cur = con.cursor()
+        encoded = request.form["password"].encode()
+        hash = hashlib.sha256(encoded).hexdigest()
+        print(hash)
         cur.execute("SELECT * FROM users WHERE username = ? AND password = ?", 
-                            (request.form["username"], request.form["password"]))
+                            (request.form["username"], hash))
         match = len(cur.fetchall())
         con.commit()
         con.close()
         if  match == 0:
             return "Login Failed"
         else:
-            return "Welcome" + request.form["username"]
+            session["username"] = request.form["username"]
+            return render_template('index.html')
 
 
 if __name__ == "__main__":
